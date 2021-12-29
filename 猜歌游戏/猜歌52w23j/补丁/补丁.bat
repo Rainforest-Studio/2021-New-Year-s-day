e11497c2fe41e1f7e5dcc1bc4d19c851cc8ade6b
debugubiyoulin <96475156+debugubiyoulinll@users.noreply.github.com>
2021-12-29 19:57:07 +0800

    Add files via upload

---
+++ 猜歌游戏/猜歌52w23j/补丁/补丁.bat
@@ -0,0 +1,47 @@
+color 2
+copy C:\Users\Administrator\Desktop\猜歌52w23b\补丁\msvcr100d.dll C:\Windows\SysWOW64
+@echo.------------------------------------------------------
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.------------------------------------------------------
+@echo.□□□□□□□□□□ 0%
+TIMEOUT /T 1
+@echo.■■■■□□□□□□ 40%
+TIMEOUT /T 1
+@echo.■■■■■■□□□□ 60%
+TIMEOUT /T 1
+@echo.■■■■■■■■■□ 90%
+TIMEOUT /T 1
+@echo.■■■■■■■■■■ 100%
+@echo.补丁安装完毕按任意键关闭程序
+pause
